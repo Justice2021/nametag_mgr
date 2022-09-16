d8841852f15d8fed0d6ee37125b3c2ada0b2c947
Justice <113161306+Justice2021@users.noreply.github.com>
2022-09-16 11:05:35 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,11 @@
 local storage = minetest.get_mod_storage()
 
-nametag_mgr = {debugging = false}
-
-local function debug(message)
-	if not nametag_mgr.debugging then return end
-	minetest.chat_send_all("nametag_mgr debug: "..message)
-end
+nametag_mgr = {}
 
 local function get_modifiers()   -- Load, with on-demand creation.
 	local serializedModifiers = storage:get_string("nametag_mgr.modifiers")
-	debug("Serialized modifiers loaded: "..serializedModifiers)
 	local modifiers = minetest.deserialize(serializedModifiers)
 	if not modifiers then
-		debug("No modifiers found. Initializing to empty list.")
 		modifiers = {}
 	end
 	return modifiers
@@ -20,7 +13,6 @@ end
 
 local function set_modifiers(modifiers)   -- Save.
 	local serializedModifiers = minetest.serialize(modifiers)
-	debug("Saving serializedModifiers: "..serializedModifiers)
 	storage:set_string("nametag_mgr.modifiers", serializedModifiers)
 end
 
@@ -34,7 +26,6 @@ function nametag_mgr.ensure_modifier(modifierName, prefix, suffix)
 	modifier.suffix = suffix
 	modifiers[modifierName] = modifier
 	set_modifiers(modifiers)   -- Save.
-	debug("Ensured modifier, "..modifierName..', with prefix, "'..prefix..'", and suffix, "'..suffix..'".')
 end
 
 function nametag_mgr.ensure_modifier_group(modifierName, groupName, color)
@@ -45,46 +36,29 @@ function nametag_mgr.ensure_modifier_group(modifierName, groupName, color)
 	groups[groupName] = color
 	modifiers[modifierName].groups = groups
 	set_modifiers(modifiers)   -- Save.
-	debug("Added group, "..groupName..", to modifier, "..modifierName..", with color, "..color..".")
 end
 
 function nametag_mgr.set_player_modifier_group(playerName, modifierName, group)
 	local player = minetest.get_player_by_name(playerName)
 	player:set_attribute("nametag-modifier-"..modifierName.."-group", group)
-	debug("Set "..playerName.."'s "..modifierName.." group to "..group..".")
 end
 
 minetest.register_on_chat_message(function(playerName, message)
-	if (minetest.settings:get_bool("no_chat_intercept")) then
-		debug("Skipping chat interception.")
-		return false
-	end
-	debug("Intercepting chat.")
+	if (minetest.settings:get_bool("no_chat_intercept")) then return false end
 
-	debug("Chatting player: "..playerName)
 	local player = minetest.get_player_by_name(playerName)
-	if not player then
-		debug("Player not found with get_player_by_name.")
-		return false
-	end
-	debug("Player found with get_player_by_name.")
+	if not player then return false end 
 
 	local modifiers = get_modifiers()   -- Load.
 	local nameTag = ""
 	for modifierName, modifier in pairs(modifiers) do
-		debug("Checking modifier, "..modifierName..":")
 		local group = player:get_attribute("nametag-modifier-"..modifierName.."-group")
 		if group then
-			debug("Player, "..playerName.."'s "..modifierName.." group: "..group)
-
 			local color = modifier.groups[group]
 			local changedColor = false
 			if color then
-				debug("color: "..color)
 				nameTag = nameTag..minetest.get_color_escape_sequence(color)
 				changedColor = true
-			else
-				debug("No color.")
 			end
 			local prefix = modifier.prefix
 			if prefix then nameTag = nameTag..prefix end
@@ -95,15 +69,11 @@ minetest.register_on_chat_message(function(playerName, message)
 			if suffix then nameTag = nameTag..suffix end
             
 			if changedColor then nameTag = nameTag..minetest.get_color_escape_sequence("#ffffff") end
-		else
-			debug("Player, "..playerName..", does not have a "..modifierName.." modifier.")
 		end
 	end
 
-	debug("nameTag: "..nameTag)
 	nameTag = playerName..nameTag..": "
 
-	debug("Sending chat message.")
 	minetest.chat_send_all(nameTag..message)
 	return true
 end)
